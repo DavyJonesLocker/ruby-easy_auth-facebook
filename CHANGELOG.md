a25331a8dc3ca24c142beb956cfc9c4972277760
Brian Cardarella <bcardarella@gmail.com>
2014-03-11 04:03:03 -0400

    Added Facebook client via Koala gem

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,4 @@
 ## EasyAuth-Facebook 0.2.0
 
 * Works with latest EasyAuth
+* Added Facebook client via Koala gem
--- lib/easy_auth/models/identities/oauth2/facebook.rb
+++ lib/easy_auth/models/identities/oauth2/facebook.rb
@@ -1,33 +1,44 @@
+require 'koala'
+
 module EasyAuth::Models::Identities::Oauth2::Facebook
-  def account_attributes_map
-    { :username => 'username', :email => 'email', :first_name => 'first_name', :last_name => 'last_name' }
-  end
+  extend ActiveSupport::Concern
 
-  def authorize_url
-    '/dialog/oauth'
+  def client
+    @client ||= Koala::Facebook::API.new(self.token)
   end
+  alias :graph :client
 
-  def oauth2_scope
-    'email'
-  end
+  module ClassMethods
+    def account_attributes_map
+      { :username => 'username', :email => 'email', :first_name => 'first_name', :last_name => 'last_name' }
+    end
 
-  def account_attributes_url
-    'https://graph.facebook.com/me'
-  end
+    def authorize_url
+      '/dialog/oauth'
+    end
 
-  def token_url
-    'https://graph.facebook.com/oauth/access_token'
-  end
+    def oauth2_scope
+      'email'
+    end
 
-  def site_url
-    'https://www.facebook.com'
-  end
+    def account_attributes_url
+      'https://graph.facebook.com/me'
+    end
 
-  def retrieve_uid(account_attributes)
-    account_attributes['id']
-  end
+    def token_url
+      'https://graph.facebook.com/oauth/access_token'
+    end
 
-  def token_options(callback_url)
-    super.merge(:parse => :query)
+    def site_url
+      'https://www.facebook.com'
+    end
+
+    def retrieve_uid(account_attributes)
+      account_attributes['id']
+    end
+
+    def token_options(callback_url)
+      super.merge(:parse => :query)
+    end
   end
 end
